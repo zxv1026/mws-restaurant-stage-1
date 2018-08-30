b24ff47b2b05f422c9d076f9328847ed5317f5cc
张晓钒 <zxv970209@outlook.com>
2018-08-31 00:28:42 +0800

    feature： 为所有图片真加了alt

--- js/main.js
+++ js/main.js
@@ -161,6 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute("alt", `${restaurant.name}'s photo`);
   li.append(image);
 
   const name = document.createElement('h1');
