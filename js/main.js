d1baf1b3959cd032564e6a7189341a8a83aa53e6
张晓钒 <zxv970209@outlook.com>
2018-09-02 11:26:17 +0800

    style：格式修改

--- js/main.js
+++ js/main.js
@@ -164,7 +164,7 @@ createRestaurantHTML = (restaurant) => {
   image.setAttribute("alt", `${restaurant.name}'s photo`);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
